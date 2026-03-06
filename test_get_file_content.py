883579ba5cc10a753c48c00b271d4c3371fe01eb
namanjain2005 <bwrnaman2005@gmail.com>
2026-03-06 21:46:07 +0530

    added more tools

---
+++ test_get_file_content.py
@@ -0,0 +1,26 @@
+from functions.get_file_content import get_file_content
+
+def test():
+    result = get_file_content("calculator", "lorem.txt")
+    print("Result for calculator/lorem.txt")
+    print(result)
+    print("")
+
+    result = get_file_content("calculator", "main.py")
+    print("Result for calculator/main.py")
+    print(result)
+
+    result = get_file_content("calculator", "pkg/calculator.py")
+    print("Result for pkg/calculator.py")
+    print(result)
+
+    result = get_file_content("calculator", "/bin/cat")
+    print("Result for bin/cat :")
+    print(result)
+
+    result = get_file_content("calculator", "pkg/does_not_exist.py")
+    print("Result for pkg/doesnotexist:")
+    print(result)
+
+if __name__ == "__main__":
+    test()
